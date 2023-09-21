7a6f900c614e6226423e05c2cbc3b48db3e32697
alfaromeo13 <jovanvukovic09@gmail.com>
2023-09-21 22:52:52 +0200

    commit.

--- src/main/java/com/companyname/demo/controllers/UserController.java
+++ src/main/java/com/companyname/demo/controllers/UserController.java
@@ -1,5 +1,15 @@
 package com.companyname.demo.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/users/")
 public class UserController {
 
+    @GetMapping("hello")
+    ResponseEntity<String> getMessage(){
+        return new ResponseEntity<>("Hello message.", HttpStatus.OK);
+    }
 }
