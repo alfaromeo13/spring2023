7b6023c9bcd43690391bf2dbc31f60a11cb8c3d3
alfaromeo13 <jovanvukovic09@gmail.com>
2023-09-28 19:10:25 +0200

    just added some changes to project structure.

--- src/main/java/com/companyname/demo/entities/Category.java
+++ src/main/java/com/companyname/demo/entities/Category.java
@@ -1,4 +1,27 @@
 package com.companyname.demo.entities;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    private String description;
+
+    @ToString.Exclude
+    @JsonManagedReference
+    @OneToMany(mappedBy = "category")
+    private List<Product> products = new ArrayList<>();
 }
